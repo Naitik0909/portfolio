ada6841353005a83bdac5ffc56bbd90f1f6e76d6
Naitik0909 <parmarnaitik0909@gmail.com>
2020-07-04 00:30:30 +0530

    js and css added

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$('#fb').hover(function(){
+  $(this).css('background-color', '#3B5998');
+})
+
+$('#insta').hover(function(){
+  $(this).css('background-color', '#D62B88');
+
+})
+
+$('#linkedin').hover(function(){
+  $(this).css('background-color', '#0077B5');
+})
